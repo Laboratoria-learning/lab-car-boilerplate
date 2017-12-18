ddd4d86fcec8eb2f3abe45b455f5fe624a64a322
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2017-12-18 05:35:04 -0500

    api maps js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function () {
 
     function initMap() {
 
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+        var location = new google.maps.LatLng(-12.125913, -77.020860);
 
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
@@ -18,5 +18,3 @@ $(function () {
 
     google.maps.event.addDomListener(window, 'load', initMap);
 });
-
-
